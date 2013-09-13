e55a33877da50ad917fb260168948ab83fc89161
shawntoffel <shawn.toffel@gmail.com>
2013-09-12 20:28:46 -0600

    progress.flac (can drag kuroko)

--- main.lua
+++ main.lua
@@ -3,6 +3,13 @@ Kuroko = {
 	y = 0,
 	w = 100,
 	h = 100,
+
+	dragging = {
+	 grabbed = false,
+	 grabX = 0,
+	 grabY = 0
+	},
+
 	collide = function(self, cx, cy)
 		return (cx - self.x < self.w) and (cy - self.y < self.h)
 	end,
@@ -23,24 +30,31 @@ end
 function love.draw()
    --love.graphics.draw(kuroko, Kuroko.x, Kuroko.y)
    Kuroko:draw()
+--[[
 	 if( Kuroko:collide(love.mouse.getX(), love.mouse.getY()) )
 	 then
 		-- Kuroko:draw()
 	 end
-
+]]
 end
 
 function love.mousepressed(x, y, button)
   if( Kuroko:collide(x, y))
 	then
-		 Kuroko.grabbed = true
-		 Kuroko.grabX = x
-		 Kuroko.grabY = y
+		 Kuroko.dragging.grabbed = true
+		 Kuroko.dragging.grabX = x - Kuroko.x
+		 Kuroko.dragging.grabY = y - Kuroko.y
+	end
+end
+
+function love.update(dt)
+	if(Kuroko.dragging.grabbed)
+	then
+		Kuroko.x = love.mouse.getX() - Kuroko.dragging.grabX
+		Kuroko.y = love.mouse.getY() - Kuroko.dragging.grabY
 	end
 end
 
 function love.mousereleased(x, y, button)
-	Kuroko.x = Kuroko.x + x - Kuroko.grabX
-	Kuroko.y = Kuroko.y + y - Kuroko.grabY
-	Kuroko.grabbed = false
+	Kuroko.dragging.grabbed= false
 end
