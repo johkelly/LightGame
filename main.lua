9403f723413bd8d65b9fe21ec1a70cd118d35d78
KinectTeam <jack@Puddle-Jumper.(none)>
2013-09-12 18:18:07 -0600

    Very pointless collision detection with Kuroko

--- main.lua
+++ main.lua
@@ -1,8 +1,22 @@
+Kuroko = {
+	x = 0,
+	y = 0,
+	w = 100,
+	h = 100,
+	collide = function(self, cx, cy)
+		return (cx - self.x < self.w) and (cy - self.y < self.h)
+	end
+};
+
 function love.load()
    kuroko = love.graphics.newImage("images/kuroko.png")
    x = 100
    y = 100
+   Kuroko.w = kuroko:getWidth()
+   Kuroko.h = kuroko:getHeight()
 end
+
 function love.draw()
-   love.graphics.draw(kuroko, x, y)
+   love.graphics.draw(kuroko, Kuroko.x, Kuroko.y)
+   print( Kuroko:collide(love.mouse.getX(), love.mouse.getY()) )
 end
