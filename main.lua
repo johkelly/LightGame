fb9080c5168ef714bda7ba2cf2b6111975119956
shawntoffel <shawn.toffel@gmail.com>
2013-09-24 18:02:40 -0600

    added item toolbox
    get from item box if below board

--- main.lua
+++ main.lua
@@ -1,21 +1,26 @@
 function love.load()
-	love.graphics.setMode(600, 500)
+	love.graphics.setMode(600, 600)
 	GridModule = require("gridspace_module")
 	MouseGrabStack = require("mousegrab_module")
-	foo = GridModule.GridContainer(25, 30, 550, 440, 5, 4)
+	board = GridModule.GridContainer(25, 30, 550, 440, 5, 4)
+	itembox = GridModule.GridContainer(25, 500, 550, 90, 5, 2)
 	kuroko = love.graphics.newImage("images/kuroko.png")
 	GO_Kuroko = GridModule.GridObject(kuroko)
-	foo:putObject(GO_Kuroko, 0, 1)
+	itembox:putObject(GO_Kuroko, 0, 0)
 end
 
 function love.draw()
-	foo:draw()
+	board:draw()
+	itembox:draw()
 	MouseGrabStack.draw()
 end
 
 function love.mousepressed(x, y, button)
-	grabbed = foo:pickObject(x, y)
-	foo:removeObjectAt(x, y)
+	area = (y > board.height) and itembox or board
+
+	grabbed = area:pickObject(x, y)
+	area:removeObjectAt(x, y)
+
 	MouseGrabStack.grab(grabbed, x, y)
 end
 
@@ -26,7 +31,7 @@ end
 function love.mousereleased(x, y, button)
 	local released, grabX, grabY = MouseGrabStack.release()
 	if released then
-		foo:snapObjectAt(released, love.mouse.getX(), love.mouse.getY()) 
+		board:snapObjectAt(released, love.mouse.getX(), love.mouse.getY()) 
 	end
 end
 
